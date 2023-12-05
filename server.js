9a7f17d622713ffe15d0f0b68bf532e138bd5fc3
Nathan Call <37190397+ncall404@users.noreply.github.com>
2023-12-04 21:01:17 -0700

    Revise links/routing
    Revised the links and routing to clean it up and to give any invalid urls an error 404 page with a link to the home page. Started making an express session for the login system to have persistence across pages.

--- server.js
+++ server.js
@@ -4,15 +4,21 @@ server.js is for running the server and handling the routing of the website. Han
 
 const express = require('express');
 const { body, matchedData, validationResult } = require('express-validator');
-const path = require("path");
-const user = require("./server-scripts/user");
-const custValidation = require("./server-scripts/custom-validation");
+const session = require('express-session');
+const path = require('path');
+const user = require('./server-scripts/user');
+const custValidation = require('./server-scripts/custom-validation');
 
 const app = express();
 const port = 8080;
 
 app.use(express.static('./'));
 app.use(express.urlencoded({extended:true}));
+app.use(session({
+    secret: 'placeholderSecret',
+    resave: false,
+    saveUninitialized: true
+})); // In production the secret would be replaced with something like an environment variable and would be much more secure. Session would possibly be stored in a database. For our purposes in this project, this placeholder will suffice. Additionaly, in production you would probably want to use something like Passport.js in conjunction with express-sessions for higher security.
 
 
 // Routing for GET requests to the server through various directories.
@@ -20,26 +26,33 @@ app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.get("/pages/", function (req, res) {
-    res.redirect('/index.html');
-});
-
-app.get("/pages/user/", function (req, res) {
-    res.redirect('/pages/user/login.html');
-});
-
 app.get("/pages/user/login", function (req, res) {
-    res.redirect('/pages/user/login.html');
+    res.sendFile(path.join(__dirname, '/pages/user/login.html'));
 });
 
 app.get("/pages/user/register", function (req, res) {
-    res.redirect("/pages/user/register.html");
+    res.sendFile(path.join(__dirname, '/pages/user/register.html'));
 });
 
-// End GET routing.
+app.get("/pages/events/event-create", function (req, res) {
+    if (!req.session.user) {
+        res.sendFile(path.join(__dirname, '/pages/events/event-create.html'));
+    } else {
+        res.sendStatus(401).send();
+    } 
+});
 
-// Routing for POST requests.
-    // User registration POST request.
+app.get("/pages/events/event-list", function (req, res) {
+    if (!req.session.user) {
+        res.sendFile(path.join(__dirname, '/pages/events/event-list.html'));
+    } else {
+        res.sendStatus(401).send();
+    }
+});
+
+
+
+// User registration POST request.
 app.post("/pages/user/register",
     body('firstName').notEmpty().withMessage("A first name is required.").trim().isAlpha().withMessage("First name must be alphabet letters.").escape(), // End of first name validation.
     body('lastName').notEmpty().withMessage("A first name is required.").trim().isAlpha().withMessage("First name must be alphabet letters.").escape(), // End of last name validation.
@@ -63,7 +76,7 @@ function(req, res) {
         const data = matchedData(req);
         user.registerUser(data.firstName, data.lastName, data.email, data.password);
 
-        res.redirect('/pages/user/login.html');
+        res.redirect('/pages/user/login');
     }
     // else if errors then send error messages to client and stay on the registration page.
     else if (errors.array().length > 0) {
@@ -72,8 +85,7 @@ function(req, res) {
     }
 });
 
-    // User login POST request.
-// ===== TODO: login post request =====
+// User login POST request.
 app.post("/pages/user/login",
     body('email').notEmpty().withMessage("An email address is required for login.").trim().toLowerCase().escape().isEmail().withMessage("Email address is incorrectly formatted. Example of correct email: ExampleAddress@email.com").custom(value => user.doesUserExist(value).then(results => {
         exists = custValidation.checkUser(results);
@@ -90,7 +102,7 @@ function(req, res) {
     if (errors.array().length <= 0) {
         const data = matchedData(req);
 
-        res.redirect('/pages/events/event-list.html');
+        res.redirect('/pages/events/event-list');
     }
     // else if errors then send error messages to client and stay on the login page.
     else if (errors.array().length > 0) {
@@ -99,14 +111,17 @@ function(req, res) {
     }
 });
 
-    // Event creation POST request.
+// Event creation POST request.
 // ===== TODO: event creation post request =====
 
-// End POST routing.
 
+// Redirect all invalid urls/routes to home screen.
+app.all('*', function(req, res) {
+    res.status(404).send('Page not found! <a href="/">Click here to go to the Home Page.</a>');
+})
 
 
 // Start Server
 app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
